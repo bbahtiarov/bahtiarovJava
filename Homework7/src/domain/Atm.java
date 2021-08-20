fc3ca7853e4e272ee3be40b308be480d193368bb
bbahtiarov <bahtiarovbogdan@yandex.ru>
2021-08-20 23:07:22 +0200

    Fix homework 7

--- Homework7/src/domain/Atm.java
+++ Homework7/src/domain/Atm.java
@@ -2,18 +2,13 @@ package domain;
 
 public class Atm {
 
-    private int quantityBanknotes20;
-    private int quantityBanknotes50;
-    private int quantityBanknotes100;
+    private final int quantityBanknotes20;
+    private final int quantityBanknotes50;
+    private final int quantityBanknotes100;
 
-    private int quantityBanknotesWithdraw20;
-    private int quantityBanknotesWithdraw50;
-    private int quantityBanknotesWithdraw100;
-
-    private final int banknotes20 = 20;
-    private final int banknotes50 = 50;
-    private final int banknotes100 = 100;
-    private boolean operation;
+    private final int[] nominalBanknotes = {20, 50, 100}; //массив имеющихся купюр
+    private final int[] quantitiesWithdrawBanknotes = {0, 0, 0};
+    private int[] quantitiesBanknotes;
 
     public Atm(int quantityBanknotes20, int quantityBanknotes50, int quantityBanknotes100) {
         this.quantityBanknotes20 = quantityBanknotes20;
@@ -21,103 +16,74 @@ public class Atm {
         this.quantityBanknotes100 = quantityBanknotes100;
     }
 
+    public int[] getNominalBanknotes() {
+        return nominalBanknotes;
+    }
+
+    public int[] getQuantitiesWithdrawBanknotes() {
+        return quantitiesWithdrawBanknotes;
+    }
+
+    public int[] getQuantitiesBanknotes() {
+        return quantitiesBanknotes;
+    }
+
     public boolean addMoney(int sum) {
 
+        boolean operation = true;
+
         if (sum % 10 != 0 || sum == 0) {
+
             operation = false;
+            System.out.println("\n" + new Exception("Incorrect sum"));
 
         } else {
-            while (sum > 0) {
-                sum -= banknotes100;
-                quantityBanknotes100++;
+
+            quantitiesBanknotes = new int[]
+                    {
+                            quantityBanknotes20,
+                            quantityBanknotes50,
+                            quantityBanknotes100
+                    };
+
+            int index = nominalBanknotes.length - 1;
+
+            while (index != -1) {
+                quantitiesBanknotes[index] = quantitiesBanknotes[index] + (sum / nominalBanknotes[index]);
+                sum %= nominalBanknotes[index];
+                index--;
             }
-            if (sum < 0) {
-                sum += banknotes100;
-                quantityBanknotes100--;
-            }
-            while (sum > 0) {
-                sum -= banknotes50;
-                quantityBanknotes50++;
-            }
-            if (sum < 0) {
-                sum += banknotes50;
-                quantityBanknotes50--;
-            }
-            while (sum > 0) {
-                sum -= banknotes20;
-                quantityBanknotes20++;
-            }
-            operation = true;
+
         }
 
-        showQuantityBanknotes(
-                quantityBanknotes20,
-                quantityBanknotes50,
-                quantityBanknotes100
-        );
         return operation;
     }
 
     public boolean withdrawMoney(int sum) {
-        int quantityAllBanknotes = banknotes20 * quantityBanknotes20
-                + banknotes50 * quantityBanknotes50
-                + banknotes100 * quantityBanknotes100;
+
+        boolean operation = true;
+        int quantityAllBanknotes =
+                20 * quantitiesBanknotes[0]
+                        + 50 * quantitiesBanknotes[1]
+                        + 100 * quantitiesBanknotes[2];
 
         if (quantityAllBanknotes < sum || sum % 10 != 0 || sum == 0) {
+
             operation = false;
             System.out.println("\n" + new Exception("Not enough money"));
+
         } else {
-            while (sum > 0 && quantityBanknotes100 > 0) {
-                sum -= banknotes100;
-                quantityBanknotes100--;
 
-                quantityBanknotesWithdraw100++;
-            }
-            if (sum < 0) {
-                sum += banknotes100;
-                quantityBanknotes100++;
+            for (int i = nominalBanknotes.length - 1; i >= 0; --i)
+            {
+                quantitiesWithdrawBanknotes[i] = sum / nominalBanknotes[i];
+                quantitiesBanknotes[i] = quantitiesBanknotes[i] - quantitiesWithdrawBanknotes[i];
+                sum = sum - nominalBanknotes[i] * quantitiesWithdrawBanknotes[i];
             }
 
-            while (sum > 0 && quantityBanknotes50 > 0) {
-                sum -= banknotes50;
-                quantityBanknotes50--;
-
-                quantityBanknotesWithdraw50++;
-            }
-            if (sum < 0) {
-                sum += banknotes50;
-                quantityBanknotes50++;
-            }
-
-            while (sum > 0 && quantityBanknotes20 > 0) {
-                sum -= banknotes20;
-                quantityBanknotes20--;
-
-                quantityBanknotesWithdraw20++;
-            }
-
-            operation = true;
         }
 
-        showQuantityBanknotes(
-                quantityBanknotesWithdraw20,
-                quantityBanknotesWithdraw50,
-                quantityBanknotesWithdraw100
-        );
-
         return operation;
-    }
-
-    private void showQuantityBanknotes(
-            int amountBanknotes20,
-            int amountBanknotes50,
-            int amountBanknotes100
-    ) {
-        System.out.println(
-                "banknotes 20: " + amountBanknotes20 + "\n"
-                        + "banknotes 50: " + amountBanknotes50 + "\n"
-                        + "banknotes 100: " + amountBanknotes100
-        );
     }
 
 }
--- Homework7/src/domain/Main.java
+++ Homework7/src/domain/Main.java
@@ -12,11 +12,46 @@ public class Main {
         Scanner addAmountScanner = new Scanner(System.in);
         int inputAddAmount = addAmountScanner.nextInt();
         atm.addMoney(inputAddAmount);
+        showAtmBalance(atm);
 
         System.out.println("Введите количество денег для снятия из банкомата.");
         Scanner wdAmountScanner = new Scanner(System.in);
         int inputWithdrawAmount = wdAmountScanner.nextInt();
         atm.withdrawMoney(inputWithdrawAmount);
+        showQuantitiesBanknotes(atm);
+
+    }
+
+    private static void showAtmBalance(Atm atm) {
+
+        for (int i = 0; i < atm.getQuantitiesBanknotes().length; i++) {
+
+            System.out.printf(
+                    "Номинал в %d руб. = %d шт.\n",
+                    atm.getNominalBanknotes()[i],
+                    atm.getQuantitiesBanknotes()[i]
+            );
+
+        }
+    }
+
+    private static void showQuantitiesBanknotes(Atm atmTest) {
+
+        for (int i = 0; i < atmTest.getNominalBanknotes().length; i++)
+        {
+
+            if (atmTest.getQuantitiesWithdrawBanknotes()[i] == 0)
+            {
+                continue;
+            }
+
+            System.out.print(
+                    "Выдано " + atmTest.getQuantitiesWithdrawBanknotes()[i]
+                            + " купюр номинала "
+                            + atmTest.getNominalBanknotes()[i] + "\n"
+            );
+
+        }
 
     }
 
